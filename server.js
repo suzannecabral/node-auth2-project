7798b73dac0a5d01e6bde9f35d1d4ce266412e16
Suzanne Cabral <suzannecabral@live.com>
2020-11-12 21:13:40 -0800

    server initialized

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const express = require('express');
 //new server
 const server = express();
 
+//use middleware
+server.use(express.json());
+//use routers
 
 
+//default endpoint
+server.get('/', (req,res,next)=>{
+  res.status(200).json({message:`The server is running, better go catch it`});
+});
 
 
 //exports
