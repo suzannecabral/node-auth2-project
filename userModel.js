135b3bc7f6358b902ad7b6cf832d158d0009c578
Suzanne Cabral <suzannecabral@live.com>
2020-11-12 22:15:15 -0800

    added basic endpoints, no auth yet

--- userModel.js
+++ userModel.js
@@ -1,11 +1,24 @@
 const db = require('./data/dbConfig');
 
 module.exports = {
-  register(newUser){
-
+  getAll(){
+    return db('users');
   },
-  login(userData){
+  getById(userId){
+    return db('users')
+      .where('users.id',userId)
+      .first();
+  },
+  getByName(username){
+    return db('users')
+      .where('users.username',username)
+      .first();
+  },
+  async addNew(newUser){
+    const [id] = await db('users').insert(newUser);
     
-  }
-
+    return db('users')
+      .where('users.id', id)
+      .first();
+  },
 }
--- userRouter.js
+++ userRouter.js
@@ -6,9 +6,58 @@ const Users = require('./userModel');
 //new router
 const router = express.Router();
 
-//ENDPOINTS: localhost:9000/api/users
+//ENDPOINTS: localhost:9000/api/
 //----------------------------------
 
+//POST: /api/login -----------------
+router.post('/login', (req, res)=>{
+  
+  const {username, password} = req.body;
+
+  Users.getByName(username)
+    .then(user =>{
+      //match user auth
+
+      //if good, then set auth status
+
+      //then welcome user with status message
+      res.status(200).json({ message: `Welcome, ${username}!` });
+    })
+    .catch(err =>{
+      console.log(err);
+      res.status(500).json({message:'Server error logging in'});
+    })
+
+});
+
+//POST: /api/register --------------
+router.post('/register', (req, res)=>{
+
+  Users.addNew(req.body)
+    .then(user=>{
+      res.status(201).json(user);
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({message:'Error getting registering new user'})
+    })
+
+});
+
+
+//GET: /api/userlist --------------
+router.get('/userlist', (req, res)=>{
+  
+  Users.getAll()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({message:'Error getting user list'})
+    })
+
+});
 
 
 //default endpoint------------------
